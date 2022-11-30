2ac5fe93611219a10c91d7ecfafdd149f63ab093
shivam yadav <58086472+shivam20yadav@users.noreply.github.com>
2022-11-30 10:01:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ class generatePdfClass{
         });
         const page = await browser.newPage();
         await page.setContent(html_code, {});
-        console.log(options)
         return Promise.resolve(page.pdf(options)).then(async function(data) {
             await browser.close();
             return data;
